8ffdffce3e78153f07c8c7c86f6aecaff0e70e0e
Tanzim Ahmed <utshotanzim@gmail.com>
2022-12-17 13:47:52 +0600

    app js file inputed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import logo from "./logo.svg";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Blog from "./components/Blog";
+import Courses from "./components/Courses";
+import LoginBootstrap from "./components/LoginBootstrap";
+import RegisterReactBootstrap from "./components/RegisterReactBootstrap";
+import Main from "./layout/Main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main>0</Main>,
+    children: [
+      {
+        path: "/",
+        element: <RegisterReactBootstrap></RegisterReactBootstrap>,
+      },
+      {
+        path: "/register",
+        element: <RegisterReactBootstrap></RegisterReactBootstrap>,
+      },
+      {
+        path: "/login",
+        element: <LoginBootstrap></LoginBootstrap>,
+      },
+      {
+        path: "/course",
+        element: <Courses></Courses>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
